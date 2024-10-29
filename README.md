a828049c654cc4e52f460ab5d14287742b7acccc
DarshanAguru <agurudf@gmail.com>
2024-10-29 19:14:10 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tools
+
+This repo has all the tools I use for my work. I'll try to keep it up to date.
---
+++ WEBDEV/tools.js
@@ -0,0 +1,11 @@
+const toolsList = [
+    {
+        id: 1,
+        name: "blog-json builder",
+        location: "./tools/blog-json-builder",
+        description: "A tool to build a json file for blogging purposes", 
+        href: "./tools/blog-json-builder/blog-json-builder.html"
+    }
+]
+
+export default toolsList;
